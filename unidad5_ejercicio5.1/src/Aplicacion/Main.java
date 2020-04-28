3bc6ea5b4604a5c5827cd56527d85b637791c328
Daniel Perez <plupyt_dany@hotmail.com>
2020-04-28 10:13:24 -0500

    imprime matriz con arcos pero todavía no es simétrica

--- unidad5_ejercicio5.1/src/Aplicacion/Main.java
+++ unidad5_ejercicio5.1/src/Aplicacion/Main.java
@@ -14,13 +14,39 @@ import Grafo.*;
  * @author plupy
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        //vars
         int totalVertices;
+        String nom;
+        int numArcos;
         Scanner entrada = new Scanner(System.in);
+        
+        //inicio
         System.out.println("Formemos un grafo");
         System.out.println("Ingresa el número de vértices que tendrá tu grafo:");
         totalVertices = entrada.nextInt();
         GrafoMatriz grafito = new GrafoMatriz(totalVertices);
+        
+        //num vertices
+        for (int i = 1; i <= totalVertices; i++) {
+            System.out.println("Ingresa el nombre del vértice " + i);
+            nom = entrada.next();
+            grafito.nuevoVertice(nom);
+        }
+        
+        //num arcos
+        System.out.println("¿cuántos arcos quieres agregar?");
+        numArcos = entrada.nextInt();
+        for (int i = 0; i < numArcos; i++) {
+            System.out.println("Ingresa de quién a quién irá el nuevo arco: ");
+            String[] vertices = new String[2];
+            for (int j = 0; j < 2; j++) {
+                vertices[j] = entrada.next();
+            }
+            grafito.nuevoArco(vertices[0], vertices[1]);
+        }
+        
+        //impresion de matriz
         int[][] Matrix = grafito.getMatriz();
         grafito.printMatAd(Matrix, totalVertices);
     }
